3ce20a56ce86fe1a17f0986ea6114c308593bb5b
ZEESHAN KHAN <162082741+ZEESHAN8692@users.noreply.github.com>
2025-02-06 18:57:10 +0530

    Prectice

---
+++ list.py
@@ -0,0 +1,115 @@
+# a= ["zeeshan","khan"]
+
+# print (type(a))
+
+# a.append("Anaiza")
+# print(a)
+
+# a.sort()
+# print(a)
+
+# a.insert(1, "Rehan Khan")
+# print(a)
+
+
+# a= ["zeeshan","khan"]
+# b= ["Rehan Khan", "Aasif Khan"]
+
+# a.extend(b)
+# print(a)
+
+
+# Vthislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+# Vthislist.remove("banana")
+
+# Vthislist.pop()
+# print(Vthislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# del thislist[0]
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.clear()
+# print(thislist)
+
+
+# Vthislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+
+# for i in Vthislist:
+#     print(i)
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#   print(thislist[i])
+
+# thislist = ["apple", "banana", "cherry"]
+# i = 0
+
+# while i < len(thislist):
+#     print(thislist[i])
+#     i= i+1
+
+
+# while i < len(thislist):
+#   print(thislist[i])
+#   i = i + 1
+
+
+# thislist = ["apple", "banana", "cherry"]
+# [print(x) for x in thislist]
+# [print(i) for i in thislist]
+
+# thislist = ["apple", "banana", "cherry"]
+# fruite = []
+# for i in thislist:
+#     fruite.append(i)
+
+# print(fruite)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwia", "mango"]
+# for i in fruits:
+#     if("a" in i ):
+#         print(i)
+
+# newlist = [x for x in fruits if "a" in x]
+
+# print(newlist)
+
+# a="hello"
+# print(a.upper())
+
+# thislist = [100, 50, 65, 82, 23]
+# thislist.sort()
+# print(thislist)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist.copy()
+# print(mylist)
+
+# thislist = ["apple", "banana", "cherry"]
+# mylist = thislist[:]
+# print(mylist)
+
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+
+# list3 = list1 + list2
+# print(list3)
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# for x in list2:
+#   list1.append(x)
+
+# print(list1)
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# list1.extend(list2)
+# print(list1)
